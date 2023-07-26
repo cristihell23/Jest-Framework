134f5ada79a858248e884ecf378984fe337817a6
Cristian Hanganu <cristihell23@yahoo.com>
2023-07-26 14:14:04 +0000

    JSDOM and fs module

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -2,10 +2,16 @@
  * @jest-environment jsdom
  */
 
+// NPM i jest-nevironment-jsdom as installing command (Jest-28 does not include JSDOM)
+
 const buttonClick = require("../button");
 
 beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html','utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
@@ -13,5 +19,8 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par")
             .innerHTML).toEqual("You Clicked");
+    });
+    test('H1 should exist', () => {
+        expect(document.getElementsByTagName('h1').length).toBe(1);
     });
 });
